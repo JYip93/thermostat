150987b8066820fbe775ca660c0541f802adb650
Johnny Yip <jyippy2012@gmail.com>
2020-10-06 15:27:00 +0100

    Added temperature and up

---
+++ spec/ThermostatSpec.js
@@ -0,0 +1,20 @@
+"use strict";
+
+describe("Thermostat", function(){
+    describe("Temperature", function(){
+        it("Thermostat starts at 20 degrees", function(){
+            let thermostat = new Thermostat
+            expect(thermostat.temperature).toBe(20);
+        });
+    });
+
+    describe("Adjust temperature", function(){
+        describe("Up will increase the temperature", function(){
+            it("Temperature will be 30 degrees", function(){
+            let thermostat = new Thermostat
+            thermostat.up(10)
+            expect(thermostat.temperature).toBe(30);
+            });
+        });
+    });
+});
